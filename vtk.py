83c8289111391884af4a0593f8d697a6616b5bbe
Sergey Litvinov <slitvinov@gmail.com>
2024-09-27 19:04:01 +0200

    add vtk.py

---
+++ vtk.py
@@ -0,0 +1,14 @@
+import sys
+from vtkmodules.vtkCommonCore import VTK_ERROR
+from vtkmodules.vtkIOXdmf2 import vtkXdmfReader
+
+
+path = sys.argv[1]
+xr = vtkXdmfReader()
+xr.CanReadFile(path)
+xr.SetFileName(path)
+xr.UpdateInformation()
+xr.Update()
+
+ds = xr.GetOutputDataObject(0)
+print(f"vtk.py: cell, points: {ds.GetNumberOfCells()}, {ds.GetNumberOfPoints()}")
