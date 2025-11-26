25501e12b7e4d938d51e05d8f2e58ab30afe0311
Sergey Litvinov <slitvinov@seas.harvard.edu>
2025-11-26 12:48:57 +0100

    3DRectMesh.py

--- 3DRectMesh.py
+++ 3DRectMesh.py
@@ -9,8 +9,10 @@ sy_path = "3DRectMesh.y.raw"
 sz_path = "3DRectMesh.z.raw"
 
 nx, ny, nz = 10, 20, 30
+dtype = np.dtype("<f8")
+precision = dtype.itemsize
 attr = np.memmap(attr_path,
-                 dtype=np.dtype("<f8"),
+                 dtype=dtype,
                  mode="w+",
                  shape=(nx, ny, nz),
                  order="F")
@@ -19,9 +21,15 @@ for x in it:
     i, j, k = it.multi_index
     attr[i, j, k] = i + j + k
 
-sx = np.memmap(sx_path, dtype=np.dtype("<f8"), mode="w+", shape=nx)
-sy = np.memmap(sx_path, dtype=np.dtype("<f8"), mode="w+", shape=ny)
-sz = np.memmap(sx_path, dtype=np.dtype("<f8"), mode="w+", shape=nz)
+sx = np.memmap(sx_path, dtype=dtype, mode="w+", shape=nx + 1)
+sy = np.memmap(sy_path, dtype=dtype, mode="w+", shape=ny + 1)
+sz = np.memmap(sz_path, dtype=dtype, mode="w+", shape=nz + 1)
+
+beta = 2.0
+coeff = beta / np.tanh(beta)
+sx[:] = np.arange(nx + 1)
+sy[:] = ny * np.tanh(coeff * np.linspace(0.0, 1.0, ny + 1))
+sz[:] = nz * np.tanh(coeff * np.linspace(0.0, 1.0, nz + 1))
 
 with open(xdmf_path, "w") as f:
     f.write(f'''\
@@ -30,25 +38,25 @@ with open(xdmf_path, "w") as f:
   <Domain>
     <Grid>
       <Topology
-          TopologyType="3DCoRectMesh"
+          TopologyType="3DRectMesh"
           Dimensions="{nz + 1} {ny + 1} {nx + 1}"/>
       <Geometry
-          GeometryType="ORIGIN_DXDYDZ">
+          GeometryType="VXVYVZ">
         <DataItem
             Format="Binary"
-            Precision="8"
+            Precision="{precision}"
             Dimensions="{nx + 1}">
           {sx_path}
         </DataItem>
         <DataItem
             Format="Binary"
-            Precision="8"
+            Precision="{precision}"
             Dimensions="{ny + 1}">
           {sy_path}
         </DataItem>
         <DataItem
             Format="Binary"
-            Precision="8"
+            Precision="{precision}"
             Dimensions="{nz + 1}">
           {sz_path}
         </DataItem>
@@ -58,7 +66,7 @@ with open(xdmf_path, "w") as f:
           Name="u">
         <DataItem
             Format="Binary"
-            Precision="8"
+            Precision="{precision}"
             Dimensions="{nz} {ny} {nx}">
           {attr_path}
         </DataItem>
@@ -70,4 +78,7 @@ with open(xdmf_path, "w") as f:
 sys.stderr.write(f'''\
 3DRectMesh.py: {attr_path=}
 3DRectMesh.py: {xdmf_path=}
+3DRectMesh.py: {sx_path=}
+3DRectMesh.py: {sy_path=}
+3DRectMesh.py: {sz_path=}
 ''')
