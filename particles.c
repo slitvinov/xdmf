aa8802c883b757098c2a0dd43c236b1c9acec3b3
Sergey Litvinov <slitvinov@seas.harvard.edu>
2025-11-26 12:53:07 +0100

    don't need \

--- particles.c
+++ particles.c
@@ -20,35 +20,35 @@ int main(void) {
 };
   nver = sizeof ver / sizeof *ver / 3;
   printf(
-"<Xdmf\n"\
-"    Version=\"2\">\n"\
-"  <Domain>\n"\
-"    <Grid>\n"\
-"      <Topology\n"\
-"	  TopologyType=\"Polyvertex\"\n"\
-"	  Dimensions=\"%d\">\n"\
-"      </Topology>\n"\
-"      <Geometry>\n"\
+"<Xdmf\n"
+"    Version=\"2\">\n"
+"  <Domain>\n"
+"    <Grid>\n"
+"      <Topology\n"
+"	  TopologyType=\"Polyvertex\"\n"
+"	  Dimensions=\"%d\">\n"
+"      </Topology>\n"
+"      <Geometry>\n"
 "	<DataItem\n"\
-"	    Dimensions=\"%d 3\"\n"\
+"	    Dimensions=\"%d 3\"\n"
 "	    Format=\"XML\">\n",
 nver, nver);
   for (i = 0; i < 3 * nver; i++)
     printf("          %+.16e\n", ver[i]);
     printf(
-"	</DataItem>\n"\
-"      </Geometry>\n"\
-"      <Attribute\n"\
-"	  Name=\"y\">\n"\
-"	<DataItem\n"\
-"	    Dimensions=\"%d\"\n"\
+"	</DataItem>\n"
+"      </Geometry>\n"
+"      <Attribute\n"
+"	  Name=\"y\">\n"
+"	<DataItem\n"
+"	    Dimensions=\"%d\"\n"
 "	    Format=\"XML\">\n", nver);
   for (i = 0; i < nver; i++)
     printf("          %+.16e\n", ver[3 * i + 1]);
     printf(
-"	</DataItem>\n"\
-"      </Attribute>\n"\
-"    </Grid>\n"\
-"  </Domain>\n"\
+"	</DataItem>\n"
+"      </Attribute>\n"
+"    </Grid>\n"
+"  </Domain>\n"
 "</Xdmf>\n");
 }
